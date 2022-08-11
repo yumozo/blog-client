b2dcf0b8e7ab388ba3c34d70b58d7ed080a56645
1 <1>
2022-08-11 23:32:29 +0700

    styling; theme button done, animation issue still there

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -24,7 +24,7 @@ export default function Dropdown() {
       <div>
         <Menu.Button
           className="inline-flex w-full justify-center rounded-md
-            bg-black bg-opacity-20 px-2 py-2
+            bg-black bg-opacity-20 p-2
             text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
         >
           <MenuIcon
--- src/components/SwitchMode.tsx
+++ src/components/SwitchMode.tsx
@@ -1,6 +1,18 @@
 import { useEffect, useState } from 'react'
 
-SwitchMode.defaultProps = {
+const ThemeSvgIcon = ({ strokeWidth, fill, children }: any) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6 m-auto"
+      fill={fill}
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={strokeWidth}
+    >
+      {children}
+    </svg>
+  )
 }
 
 export default function SwitchMode() {
@@ -9,15 +21,6 @@ export default function SwitchMode() {
   const handleToggle = (e: any) => {
     setDarkTheme(e.target.checked)
   }
-
-  // useEffect(() => {
-  //   if (theme === 'dark') {
-  //     const switchElement = document.getElementById('switch-theme')
-  //     if (switchElement) {
-  //       switchElement.checked = true
-  //     }
-  //   }
-  // }, [])
 
   useEffect(() => {
     if (darkTheme !== undefined) {
@@ -37,60 +40,48 @@ export default function SwitchMode() {
   }, [darkTheme])
 
   return (
-    <form action="#">
-      <label className="switch flex h-auto cursor-pointer">
-        <input
-          type="checkbox"
-          onChange={handleToggle}
-          checked={darkTheme as undefined | boolean}
-          id='switch-theme'
-          className="relative hidden h-[28px] shrink-0
+    <div className=''>
+      <form action="#">
+        <label className="switch flex h-auto cursor-pointer">
+          <input
+            type="checkbox"
+            onChange={handleToggle}
+            checked={darkTheme as undefined | boolean}
+            id='switch-theme'
+            className="relative hidden h-[28px] shrink-0
             rounded-full border-2 border-transparent
             transition-colors duration-200 ease-in-out
             focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75"
-        />
-        <span className="sr-only">Switch theme</span>
-        <span
-          aria-hidden="true"
-          className={`${darkTheme ? 'text-gray-800 bg-[#fcfcfc]' : 'text-white bg-[#121212]'
-            }
-            pointer-events-none inline-block h-[28px] w-[28px]
+          />
+          <span className="sr-only">Switch theme</span>
+          <span
+            aria-hidden="true"
+            className={`${darkTheme ? 'text-gray-800 bg-[#fcfcfc]' : 'text-white bg-[#121212]'
+              }
+            pointer-events-none flex h-[36px] w-[28px]
             rounded-md shadow-lg ring-0
             transition duration-200 ease-in-out`}
-        >
-          {darkTheme ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="-2 -2 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="currentColor"
-              viewBox="-2 -2 24 24"
-              stroke="currentColor"
-              strokeWidth={0}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-              />
-            </svg>
-          )}
-        </span>
-      </label>
-    </form>
+          >
+            {darkTheme ? (
+              <ThemeSvgIcon strokeWidth={2} fill={"none"}>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              </ThemeSvgIcon>
+            ) : (
+              <ThemeSvgIcon strokeWidth={0} fill={"currentColor"}>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+              </ThemeSvgIcon>
+            )}
+          </span>
+        </label>
+      </form>
+    </div>
   )
 }
